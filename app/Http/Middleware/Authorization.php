2d6db0d2586aa703b1b5ad8c16d7abadc0108976
LEENTechDev <wapp.visuals@gmail.com>
2022-09-19 14:02:32 +0800

    first commit

---
+++ app/Http/Middleware/Authorization.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class Authorization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])){
+            $valid_passwords = array (env('AUTHORIZATION_U') => env('AUTHORIZATION_P'));
+            $valid_users = array_keys($valid_passwords);
+
+            $user = $_SERVER['PHP_AUTH_USER'];
+            $pass = $_SERVER['PHP_AUTH_PW'];
+
+            $validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
+
+            if (!$validated) {
+                header('WWW-Authenticate: Basic realm="Sophia Jewellery"');
+                header('HTTP/1.0 401 Unauthorized');
+                die ("Not authorized");
+            }
+
+            return $next($request);
+        }else{
+            header('WWW-Authenticate: Basic realm="Sophia Jewellery"');
+            header('HTTP/1.0 401 Unauthorized');
+            die ("Not authorized");
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/* ========== add here all the base api ========= */
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('generate-token', [App\Http\Controllers\AuthController::class, 'generateToken'])->name('generateToken');
+
+    Route::group(['prefix' => '', 'middleware' => ['authorization']], function () {
+
+    });
+});
+
+/* ========== for Facilito project purpose ========= */
+/* 
+    add here the custom api 
+*/
+Route::group(['prefix' => 'v2'], function () {
+    Route::post('/generate-token', 'EntregoController@authenticate')->name('generateToken2');
+});
